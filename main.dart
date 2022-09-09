a10d172c3c097d3636165366b86585ebd3094ec6
Rerison-Alves <107881260+Rerison-Alves@users.noreply.github.com>
2022-09-09 08:31:53 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,3 +1,10 @@
+import 'dart:io';
+
 void main(){
-  print('Hello World');
+  List<int> pilha = [];
+  var expressao = "5 2 + 3 7 * +";
+  var expressao2 = stdin.readLineSync();
+    for(var x in expressao2.split(' ')){
+      print(x);
+    }
 }

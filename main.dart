ea3ed75a2aea1ea9bfe6e08d5f5c637862257ad6
Rerison-Alves <107881260+Rerison-Alves@users.noreply.github.com>
2022-09-09 09:04:03 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -4,12 +4,39 @@ void main(){
   print('Digite a expressão:');
   var expressao = stdin.readLineSync();
   calcula(expressao);
-}
+} 
 
 void calcula(var expressao){
   List<int> pilha = [];
     for(var x in expressao.split(' ')){
-      print(x);
+      if(x!='+' && x!='-' && x!='*' && x!='/'){
+        pilha.add(int.parse(x));
+      }else{
+        switch(x){
+          case '+':{
+            int b = pilha.last;
+            pilha.removeLast();
+            int a = pilha.last;
+            pilha.removeLast();
+            print(a+b);
+            pilha.add(a+b);
+            }
+            break;
+          case '-':{
+              
+            }
+            break;
+          case '*':{
+              
+            }
+            break;
+          case '/':{
+              
+            }
+            break;
+        }
+        print(pilha);
+      }
     }
   
 }

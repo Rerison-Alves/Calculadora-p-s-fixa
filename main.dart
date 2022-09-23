538607d182f8b5df7750578690456aaf7c35549b
Rerison-Alves <rerison.alves07@aluno.ifce.edu.br>
2022-09-23 19:29:34 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -9,81 +9,74 @@ void main() {
 }
 
 class Calculadora {
-  List<double> pilha = List.filled(200, 1/0);
+  List<double> pilha = List.filled(200, 1 / 0);
   int top = 0;
 
   double calcula(var expressao) {
     for (var x in expressao.split(' ')) {
       if (x != '+' && x != '-' && x != '*' && x != '/') {
-        try {push(double.parse(x), pilha);}
-         catch(Exception){
-          print('DIGITE ALGO VÁLIDO');
+        try {
+          push(double.parse(x), pilha);
+        } catch (Exception) {
+          print('Expressão inválida, verifique-a');
         }
-      }
-      if(expressao.isEmpty) {
-        print('Digite alguma coisa');
-      }
-
-      else {
-        switch (x) {
-          case '+':
-            {
-              double b = pop(pilha);
-              double a = pop(pilha);
-              push(a + b, pilha);
-            }
-            break;
-          case '-':
-            {
-              double b = pop(pilha);
-              double a = pop(pilha);
-              push(a - b, pilha);
-            }
-            break;
-          case '*':
-            {
-              double b = pop(pilha);
-              double a = pop(pilha);
-              push(a * b, pilha);
-            }
-            break;
-          case '/':
-            {
-              double b = pop(pilha);
-              double a = pop(pilha);
-              push(a / b, pilha);
-            }
-            break;
-        }
+      } else {
+        try {
+          switch (x) {
+            case '+':
+              {
+                double b = pop(pilha);
+                double a = pop(pilha);
+                push(a + b, pilha);
+              }
+              break;
+            case '-':
+              {
+                double b = pop(pilha);
+                double a = pop(pilha);
+                push(a - b, pilha);
+              }
+              break;
+            case '*':
+              {
+                double b = pop(pilha);
+                double a = pop(pilha);
+                push(a * b, pilha);
+              }
+              break;
+            case '/':
+              {
+                double b = pop(pilha);
+                double a = pop(pilha);
+                push(a / b, pilha);
+              }
+              break;
+          }
+        } catch (Exception) {}
       }
     }
-    if(top>0){
+    if (top > 0) {
       print('ERRO: OPERADOR A MENOS');
       exit(0);
     }
-    if(top<0){
+    if (top < 0) {
       print('ERRO: OPERADOR A MAIS');
       exit(0);
     }
-
 
     top = 0;
     return pilha[0];
   }
 
   void push(double i, List<double> pilha) {
-    if (pilha[0] != 1/0) top++;
+    if (pilha[0] != 1 / 0) top++;
     pilha[top] = i;
   }
 
   double pop(List<double> pilha) {
     double i = pilha[top];
     pilha[top] = 0;
-
-    if (top != 1/0) top--;
-
+    if (top != 1 / 0) top--;
     return i;
-
   }
 }
-
